772b84c081072c77fcdd7b7ff420290e8c60d9f2
Rakesh Prajapati <rp151090@gmail.com>
2026-03-12 15:08:26 +0100

    ignore config file

--- includes/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'feedbackflow');
-define('DB_USER', 'root');
-define('DB_PASS', 'YourStrongPassword123!');
-define('BASE_URL', 'http://localhost/feedbackflow');
-if (session_status() === PHP_SESSION_NONE) { session_start(); }
-?>
